1ee26d320d1edc7df81784cb2087668b29cd0850
Olivier Mengué <dolmen@cpan.org>
2011-07-02 09:32:17 +0200

    Implement unshift

--- lib/Perlude/List.pm
+++ lib/Perlude/List.pm
@@ -6,13 +6,59 @@ use warnings;
 sub TIEARRAY
 {
     my ($class, $i) = @_;
-    bless $i, $class;
+    bless [ $i ], $class;
 }
 
 sub SHIFT
 {
     my $self = shift;
-    $self->()
+    $self->[0]->()
+}
+
+sub UNSHIFT
+{
+    my $self = shift;
+    push @{$self}, \@_;
+    bless $self, 'Perlude::List::Unshift';
+    undef
+}
+
+package Perlude::List::Unshift;
+
+our @ISA = 'Perlude::List';
+
+sub TIEARRAY
+{
+    die;
+}
+
+sub SHIFT
+{
+    my $self = shift;
+    #warn "# Perlude::List::Unshift::SHIFT\n";
+    my $v = shift @{$self->[1]};
+    unless (@{$self->[1]}) {
+        delete $self->[1];
+        bless $self, 'Perlude::List';
+    }
+    $v
+}
+
+sub UNSHIFT
+{
+    my $self = shift;
+    unshift @{$self->[1]}, @_;
+}
+
+BEGIN {
+    *DEBUG = $ENV{PERL_PERLUDE_LIST_DEBUG} ? sub () { 1 } : sub() { 0 };
+}
+
+# Looks like that at least Perl 5.10.1 needs this for unshift on a Perlude::List
+sub FETCHSIZE
+{
+    warn "FETCHSIZE\n" if DEBUG;
+    0;
 }
 
 1;
--- t/30-list.t
+++ t/30-list.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 5;
+use Test::More tests => 17;
 
 use Perlude;
 use Perlude::List;
@@ -11,6 +11,20 @@ use Perlude::List;
     is((shift @list), 1);
     is((shift @list), 2);
     is((shift @list), 3);
+    isa_ok tied(@list), 'Perlude::List', 'isa';
+    unshift @list, 482;
+    isa_ok tied(@list), 'Perlude::List::Unshift', 'isa';
+    is((shift @list), 482);
+    isa_ok tied(@list), 'Perlude::List', 'isa';
+    is((shift @list), 4);
+    isa_ok tied(@list), 'Perlude::List', 'isa';
+    unshift @list, 327, 624;
+    isa_ok tied(@list), 'Perlude::List::Unshift', 'isa';
+    is((shift @list), 327);
+    isa_ok tied(@list), 'Perlude::List::Unshift', 'isa';
+    is((shift @list), 624);
+    isa_ok tied(@list), 'Perlude::List', 'isa';
+    is((shift @list), 5);
     isa_ok tied(@list), 'Perlude::List', 'isa';
 }
 
